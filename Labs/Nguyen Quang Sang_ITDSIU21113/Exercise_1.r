c8211cddac1c8fc3908672daa1ae27457e3edc9c
Nguyen Quang Sang <ITDSIU21113@student.hcmiu.edu.vn>
2023-10-17 11:51:53 +0700

    new modified

--- Labs/Nguyen Quang Sang_ITDSIU21113/Exercise_1.r
+++
@@ -1,9 +0,0 @@
-#define the function taking two arguments
-bigger_100 <- function(x1, x2) {
-  #conditional
-  return (as.integer(x1 * x2 > 100))
-}
-
-#call the function to test it
-bigger_100(10, 50)
-bigger_100(10, 2)
