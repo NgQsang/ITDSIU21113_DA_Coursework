c5c3bb936311e0d9edd8e3bc9f8b784dadfe79ef
Nguyen Quang Sang <ITDSIU21113@student.hcmiu.edu.vn>
2023-10-19 17:30:10 +0700

    add the ex2_lab2

---
+++ src/labs/Nguyen Quang Sang_ITDSIU21113_lab2/Exercise_2.R
@@ -0,0 +1,12 @@
+#lap report by comment in Exercise 2
+
+#In my opinion, I think this is the untidy data
+#because for the following reason:
+
+#1) quick look at the dataset in Excel you can see that
+#the two columns "state name" and "state" are too redundant(A lot of data is duplicated)
+#the value of these two col are the same
+
+#2) quick look at, you can see that data is too wide, all the date value is messy
+#the extraneous data "X" precedes the data of the date column
+#They should be combined into 1 column, and reformat
